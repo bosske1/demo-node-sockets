cf29a8c5b07d3a549602a311d6970149bd421dc5
Laslo Horvath <bosske1@gmail.com>
2018-11-29 18:30:59 +0100

    last changes

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,30 @@
 
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as cors from "cors";
 import {Routes} from "./routes/activityRoutes";
 
 class App {
 
     public app: express.Application;
+    public router: express.Router;
     public routes: Routes = new Routes();
 
     constructor() {
         this.app = express();
+        this.router = express.Router();
         this.config();
         this.routes.routes(this.app);
+
+        const options:cors.CorsOptions = {
+            allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+            credentials: false,
+            methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+            preflightContinue: false
+        };
+
+        this.router.use(cors(options));
+        this.router.options("*", cors(options));
     }
 
     private config(): void{
